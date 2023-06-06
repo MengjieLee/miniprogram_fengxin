65a3e455dedb655e08351cf741da33131d340829
limengjie <limengjiejj@hotmail.com>
2023-06-06 12:59:09 +0800

    feat(#6): [UI] add feedback layout

--- pages/feedback/feedback.js
+++ pages/feedback/feedback.js
@@ -1,6 +1,19 @@
 Page({
   data: {
-
+    category: 'other_question',
+    categoryList: [{
+        label: '问题反馈',
+        value: 'other_question'
+      },
+      {
+        label: '社区建设',
+        value: 'region_construction'
+      },
+      {
+        label: '功能建议',
+        value: 'app_function'
+      },
+    ]
   },
   onLoad(options) {
 
@@ -19,5 +32,15 @@ Page({
     return {
       title: '',
     };
+  },
+  onCategoryChosen(e) {
+    console.log('意见反馈选择类别:', e)
+    this.setData({
+      category: e.detail.value
+    })
+  },
+  formSubmit(event) {
+    let feedbackContent = event.detail.value
+    console.log('意见反馈提交内容:', feedbackContent)
   },
 });

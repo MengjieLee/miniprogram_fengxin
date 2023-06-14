a5800e669871de4bd182d1d1a959be97b5b1f7dc
limengjie <limengjiejj@hotmail.com>
2023-06-14 21:55:25 +0800

    feat(#10): [UI] add applyParty page

--- pages/applyParty/applyParty.js
+++ pages/applyParty/applyParty.js
@@ -1,7 +1,10 @@
-Page({
-  data: {
+import {
+  customInputValidation,
+} from '../../utils/utils'
 
-  },
+
+Page({
+  data: {},
   onLoad(options) {
 
   },
@@ -22,4 +25,22 @@ Page({
       title: '',
     };
   },
+  onPhoneInput(e) {
+    const {
+      key,
+    } = e.currentTarget.dataset
+    const {
+      value
+    } = e.detail
+    console.log(e)
+    const phonePrecheckPass = customInputValidation(key, value)
+
+  },
+  getPhoneNumber(e) {
+    console.log(e)
+  },
+  formSubmit(event) {
+    let applyPartyFormData = event.detail.value
+    console.log('入党申请表单:', applyPartyFormData)
+  }
 });
